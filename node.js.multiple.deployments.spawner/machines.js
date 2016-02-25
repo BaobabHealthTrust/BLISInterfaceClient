edf14cfb3e76bf219350bc75e8a09fbf0ee19c7f
Chimwemwe Kachaje <chimwemwe.kachaje@baobabhealth.org>
2016-02-25 11:35:57 +0200

    Added a delay to the logic for launching machine threads

--- node.js.multiple.deployments.spawner/machines.js
+++ node.js.multiple.deployments.spawner/machines.js
@@ -3,17 +3,20 @@ var path = require("path");
 var exec = require('child_process').exec;
 var fs = require('fs');
 
-var files = fs.readdirSync(path.resolve("machines", "active"));
+setTimeout(function() {
+    var files = fs.readdirSync(path.resolve("machines", "active"));
 
-for(var i in files) {
+    for (var i in files) {
 
-    var child = exec('cd ' + path.resolve('machines', 'active', files[i]) + '; java -jar ' + path.resolve('machines',
-            'active', files[i], 'BLISInterfaceClient.jar'),
-        function (error, stdout, stderr) {
-            console.log('Output -> ' + stdout);
-            if (error !== null) {
-                console.log("Error -> " + error);
-            }
-        });
+        var child = exec('cd ' + path.resolve('machines', 'active', files[i]) + '; java -jar ' + path.resolve('machines',
+                'active', files[i], 'BLISInterfaceClient.jar'),
+            function (error, stdout, stderr) {
+                console.log('Output -> ' + stdout);
+                if (error !== null) {
+                    console.log("Error -> " + error);
+                }
+            });
 
-}
+    }
+
+}, 5000);
